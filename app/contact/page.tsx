1bdb2f92f9c56b8e10d86509420a6d75525f2f39
shamim-001 <s.ahsan.ds96@gmail.com>
2023-12-17 10:56:55 +0600

    modify project and contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="flex h-screen items-center justify-center text-red-500">
+      Under construction!
+    </div>
+  );
 };
 
 export default Page;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="flex h-screen items-center justify-center text-2xl text-green-500">
+      Cooming Soon!
+    </div>
+  );
 };
 
 export default Page;
