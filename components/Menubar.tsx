2deb9e1a11bcf7d4162baa7e956f06d8f07ab715
Shamim <s.ahsan.ds96@gmail.com>
2024-04-19 09:00:56 +0600

    remove layoutId from menubar li

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,28 +1,22 @@
 "use client";
 import { NAVITEMS } from "@/constants/constants";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
-import { useId } from "react";
 
 const Menubar = () => {
   const path = usePathname();
-  const layoutId = useId();
 
   return (
     <motion.nav
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       className="menu menu-vertical md:menu-horizontal"
     >
       {NAVITEMS.map((item) => (
-        <motion.li
-          layoutId={layoutId}
-          className={`${path === item.href && "active"}`}
-          key={item.href}
-        >
+        <li className={`${path === item.href ? "active" : ""}`} key={item.href}>
           <Link href={item.href}>{item.label}</Link>
-        </motion.li>
+        </li>
       ))}
     </motion.nav>
   );
